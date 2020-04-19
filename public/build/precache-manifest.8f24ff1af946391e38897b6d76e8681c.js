7633a3aeefa827e2590108172ec40703cfe9e5c0
Saksham <chauhanssaksham@gmail.com>
2020-04-19 12:24:52 +0530

    Built for deployment

---
+++ public/build/precache-manifest.8f24ff1af946391e38897b6d76e8681c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269d398548cf0496d0f47a2e2f41db08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5803cb6f3ef020bff5c3",
+    "url": "/static/css/2.885429f9.chunk.css"
+  },
+  {
+    "revision": "074ccf79fd37e4cc334c",
+    "url": "/static/css/main.dcaba755.chunk.css"
+  },
+  {
+    "revision": "5803cb6f3ef020bff5c3",
+    "url": "/static/js/2.939e7d3f.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.939e7d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074ccf79fd37e4cc334c",
+    "url": "/static/js/main.076b1e80.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/static/media/logo.950ba665.png"
+  }
+]);
