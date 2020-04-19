7b254d989f63c498b290cefccb0772f98927c585
Saksham <chauhanssaksham@gmail.com>
2020-04-19 10:03:01 +0530

    Launch Page styling

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,6 +2,8 @@ import React, {Fragment} from 'react'
 import gql from 'graphql-tag'
 import {Query} from 'react-apollo'
 import LaunchItem from './LaunchItem'
+import MissionKey from './MissionKey'
+import Skeleton from '@bit/pedox.react-skeleton.skeleton';
 
 const Launches = () => {
     const LAUNCHES_QUERY = gql`
@@ -17,12 +19,19 @@ const Launches = () => {
     return (
         <Fragment>
             <h3 className="my-3">Launches</h3>
+            <MissionKey/>  
             <Query query={LAUNCHES_QUERY}>
                 {
                     (res)=>{
                         const {loading, error, data} = res;
-
-                        if(loading) {return <h4>Loading...</h4>}
+                        if(loading) {
+                            return <Fragment>
+                            <div className="card mb-3" style={{background:"transparent"}}><Skeleton width="100%" height={120} dark /></div>
+                            <div className="card mb-3" style={{background:"transparent"}}><Skeleton width="100%" height={120} dark /></div>
+                            <div className="card mb-3" style={{background:"transparent"}}><Skeleton width="100%" height={120} dark /></div>
+                            <div className="card mb-3" style={{background:"transparent"}}><Skeleton width="100%" height={120} dark /></div>
+                        </Fragment>
+                        }
                         if(error) {console.log(error); return null;}
 
                         return <Fragment>
